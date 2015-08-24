8d47e576f2beb22088eeb834e3a7c1e9533d9bc5
Giuliano Caliari <giuliano@99taxis.com>
2015-08-24 15:31:34 -0300

    created controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,56 @@
 package controllers
 
-import play.api._
+import java.nio.charset.StandardCharsets.UTF_16LE
+
+import models.User
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.iteratee.Enumerator
 import play.api.mvc._
+import slick.backend.DatabasePublisher
+
+import scala.concurrent.Future
 
 class Application extends Controller {
 
+  val filename = "reactive_streams.csv"
+  val charset = UTF_16LE
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("WelcomeFirst created the DB then stream the results"))
   }
 
+  def createDb = Action.async {
+    User.createDb().map { r =>
+      Ok(views.html.index("DB Created!"))
+    }
+  }
+
+  def deleteDb = Action.async {
+    User.deleteDb().map { r =>
+      Ok(views.html.index("DB Deleted!"))
+    }
+  }
+
+  def csvStream = Action.async {
+    Future {
+      val userStream: DatabasePublisher[User] = User.userStream()
+      val userEnum: Enumerator[User] = play.api.libs.streams.Streams.publisherToEnumerator(userStream)
+      val byteArrayEnumerator: Enumerator[Array[Byte]] = Enumerator(csvHeader.getBytes(charset)) >>> userEnum.map(user => userToCsv(user).getBytes(charset))
+
+      Ok.chunked[Array[Byte]](byteArrayEnumerator).as(s"text/csv; charset=$charset")
+        .withHeaders(CONTENT_ENCODING -> charset.name)
+        .withHeaders(CONTENT_DISPOSITION -> s"attachment; filename=$filename")
+    }
+  }
+
+
+  private val csvHeader =
+    """sep=,
+      |"Id","Name","Document","Enabled"""".stripMargin
+
+
+  private def userToCsv(user: User): String = {
+    "\n" + List(user.id.getOrElse("--"), user.name, user.documentNumber, user.enabled).map("\"%s\"".format(_)).mkString(",")
+  }
+  
 }
