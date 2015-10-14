ff170547936397465d8f2b201f3a4d2c389c5850
Giuliano Caliari <giuliano@99taxis.com>
2015-10-14 14:15:06 -0300

    testing data creation

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,11 @@
-import org.specs2.mutable._
-import org.specs2.runner._
+import models.User
 import org.junit.runner._
-
+import org.specs2.runner._
 import play.api.test._
-import play.api.test.Helpers._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends PlaySpecification {
@@ -20,6 +22,16 @@ class ApplicationSpec extends PlaySpecification {
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
       contentAsString(home) must contain ("Welcome! First created the DB, then stream the results")
+    }
+
+    "create and populate database" in new WithApplication {
+      val home = route(FakeRequest(GET, "/create")).get
+
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
+      contentAsString(home) must contain ("DB Created and populated!")
+
+      Await.result(User.count(), 1 second) must equalTo(99999)
     }
   }
 }
