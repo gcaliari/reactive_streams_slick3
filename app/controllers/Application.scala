cb57fbbc9f3f5e53e922fde446c30dc5aa5194ea
Giuliano Caiari <giuliano.caliari@quantium.com.au>
2016-07-20 15:21:48 +1000

    fixed imports

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,6 @@
 package controllers
 
-import java.nio.charset.StandardCharsets.UTF_16LE
-
-import models.User
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.iteratee.Enumerator
 import play.api.mvc._
-import slick.backend.DatabasePublisher
-
-import scala.concurrent.Future
 
 class Application extends Controller {
 
