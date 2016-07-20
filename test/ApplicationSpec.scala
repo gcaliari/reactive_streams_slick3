7a6f15b19b9c211e27b3c647158f34908001c945
Giuliano Caiari <giuliano.caliari@quantium.com.au>
2016-07-20 16:31:44 +1000

    imports and fixed deprecated call

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,10 +1,6 @@
-import models.User
 import org.junit.runner._
 import org.specs2.runner._
 import play.api.test._
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 
 @RunWith(classOf[JUnitRunner])
@@ -13,11 +9,11 @@ class ApplicationSpec extends PlaySpecification {
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication {
-      route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+      route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
 
     "render the index page" in new WithApplication {
-      val home = route(FakeRequest(GET, "/")).get
+      val home = route(app, FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
